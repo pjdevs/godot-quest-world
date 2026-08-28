9350ec4d0c5da37ab18830ab0620946162a4af0a
pjdevs <pj33720@gmail.com>
2026-08-29 01:08:02 +0200

    feat(gameplay): add unlock sound feedback

--- quest_world/interactibles/door/Door.cs
+++ quest_world/interactibles/door/Door.cs
@@ -3,51 +3,58 @@ using QuestWorld.State;
 
 public partial class Door : Node3D
 {
-    [Export]
-    public StatefulComponent? StatefulComponent { get; set; }
+	[Export]
+	public StatefulComponent? StatefulComponent { get; set; }
 
-    [Export]
-    public AnimationPlayer? AnimationPlayer { get; set; }
+	[Export]
+	public AnimationPlayer? AnimationPlayer { get; set; }
 
-    [Export]
-    public CollisionShape3D? Collision { get; set; }
+	[Export]
+	public CollisionShape3D? Collision { get; set; }
+
+	[Export]
+	public AudioStreamPlayer3D? AudioPlayer { get; set; }
 
 
-    public override void _EnterTree()
-    {
-        StatefulComponent?.StateChanged += OnStateChanged;
-        StatefulComponent?.StateChangedPresentation += OnStateChangedPresentation;
-    }
+	public override void _EnterTree()
+	{
+		StatefulComponent?.StateChanged += OnStateChanged;
+		StatefulComponent?.StateChangedPresentation += OnStateChangedPresentation;
+	}
 
-    public override void _ExitTree()
-    {
-        StatefulComponent?.StateChanged -= OnStateChanged;
-        StatefulComponent?.StateChangedPresentation -= OnStateChangedPresentation;
-    }
+	public override void _ExitTree()
+	{
+		StatefulComponent?.StateChanged -= OnStateChanged;
+		StatefulComponent?.StateChangedPresentation -= OnStateChangedPresentation;
+	}
 
-    private void OnStateChanged(StringName oldState, StringName newState, bool isSynchronization)
-    {
-        switch (newState)
-        {
-            case "opened":
-                Collision?.Disabled = true;
-                break;
-            case "closed":
-                Collision?.Disabled = false;
-                break;
-        }
-    }
+	private void OnStateChanged(StringName oldState, StringName newState, bool isSynchronization)
+	{
+		switch (newState)
+		{
+			case "opened":
+				Collision?.Disabled = true;
+				break;
+			case "closed":
+				Collision?.Disabled = false;
+				break;
+		}
+	}
 
-    private void OnStateChangedPresentation(StringName oldState, StringName newState, bool isSynchronization)
-    {
-        switch (newState)
-        {
-            case "opened":
-                AnimationPlayer?.Play("open");
-                break;
-            case "closed":
-                AnimationPlayer?.Play(oldState == "opened" ? "close" : "RESET");
-                break;
-        }
-    }
+	private void OnStateChangedPresentation(StringName oldState, StringName newState, bool isSynchronization)
+	{
+		switch ((oldState, newState))
+		{
+			case var (state, _) when state == "locked":
+				AudioPlayer?.Play(0.1f);
+				break;
+			case var (_, state) when state == "opened":
+				AnimationPlayer?.Play("open");
+				break;
+			case var (_, state) when state == "closed":
+				AnimationPlayer?.Play(oldState == "opened" ? "close" : "RESET");
+				break;
+		}
+
+	}
 }
