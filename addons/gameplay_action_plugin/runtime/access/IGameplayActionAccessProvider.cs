dea45df4d6f38e527f28cf23efdf5e277bc87122
Pierre-Jean MOREL <pj33720@gmail.com>
2026-09-02 22:52:39 +0200

    refactor(gameplay-action): unify action access checks

--- addons/gameplay_action_plugin/runtime/access/IGameplayActionAccessProvider.cs
+++ addons/gameplay_action_plugin/runtime/access/IGameplayActionAccessProvider.cs
@@ -11,7 +11,5 @@ public readonly record struct GameplayActionAccessContext(
 
 public interface IGameplayActionAccessProvider
 {
-    bool CanRequest(in GameplayActionAccessContext context);
-
-    bool HasSustainedAccess(in GameplayActionAccessContext context) => CanRequest(context);
+    bool CanAccess(in GameplayActionAccessContext context);
 }
